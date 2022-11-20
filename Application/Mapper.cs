a6f346a68914c0a4d2828909ecf3eabbcfeaee69
Mathis <Mathisbruyere@hotmail.com>
2022-11-20 12:29:05 +0100

    commit du dto output user

---
+++ Application/Mapper.cs
@@ -0,0 +1,33 @@
+﻿
+using Application.UseCases.Users.Dto;
+using AutoMapper;
+using Domain;
+using Infrastructure.EF.DbEntities;
+
+
+namespace Application;
+
+public static class Mapper
+{
+    private static AutoMapper.Mapper _instance;
+
+    public static AutoMapper.Mapper GetInstance()
+    {
+        return _instance ??= CreateMapper();
+    }
+
+    private static AutoMapper.Mapper CreateMapper()
+    {
+        var config = new MapperConfiguration(cfg =>
+        {
+            // Source, Destination
+            // User
+            cfg.CreateMap<Users, DtoOutputUser>();
+            cfg.CreateMap<DbUser, DtoOutputUser>();
+            cfg.CreateMap<DbUser, Users>();
+            // Game
+
+        });
+        return new AutoMapper.Mapper(config);
+    }
+}
---
+++ Application/UseCases/Users/UseCaseFetchById.cs
@@ -0,0 +1,23 @@
+﻿using Application.UseCases.Users.Dto;
+using AutoMapper;
+using Infrastructure.EF.User;
+
+namespace Application.UseCases.Users;
+
+public class UseCaseFetchById
+{
+    private readonly IUsers _userRepository;
+
+
+    public UseCaseFetchById(IUsers userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    public DtoOutputUser Execute(int id)
+    {
+        var dbUser = _userRepository.FetchById(id);
+        return Mapper.GetInstance().Map<DtoOutputUser>(dbUser);
+    }
+    
+}
